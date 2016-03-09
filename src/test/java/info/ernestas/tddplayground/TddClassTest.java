f471f768495b77b1dcdcd3c5345763f18db06515
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-03-09 12:52:14 +0200

    Refactoring

--- src/test/java/info/ernestas/tddplayground/TddClassTest.java
+++ src/test/java/info/ernestas/tddplayground/TddClassTest.java
@@ -1,7 +1,9 @@
 package info.ernestas.tddplayground;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TddClassTest {
 
@@ -9,7 +11,35 @@ public class TddClassTest {
     public void testTestClassExists() {
         TddClass tddClass = new TddClass();
 
-        Assert.assertNotNull(tddClass.testMethod());
+        assertNotNull(tddClass.testMethod());
+    }
+
+    @Test
+    public void testWithZero() {
+        TddClass tddClass = new TddClass();
+
+        assertEquals(0, tddClass.sum("0"));
+    }
+
+    @Test
+    public void testWithOne() {
+        TddClass tddClass = new TddClass();
+
+        assertEquals(1, tddClass.sum("1"));
+    }
+
+    @Test
+    public void testWithTwo() {
+        TddClass tddClass = new TddClass();
+
+        assertEquals(2, tddClass.sum("2"));
+    }
+
+    @Test
+    public void testWithTwo2() {
+        TddClass tddClass = new TddClass();
+
+        assertEquals(4, tddClass.sum("1,3"));
     }
 
 }
