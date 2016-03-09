2cbc6e2e6f2cd6bcb1a8dc1a8bd4d0124c6aca81
Ernestas Kardzys <ernestas.kardzys@intermedix.com>
2016-03-09 12:52:14 +0200

    Refactoring

--- src/main/java/info/ernestas/tddplayground/TddClass.java
+++ src/main/java/info/ernestas/tddplayground/TddClass.java
@@ -5,4 +5,15 @@ public class TddClass {
     public String testMethod() {
         return "Returning not null result :-)";
     }
+
+    public int sum(String text) {
+        if (text.contains(",")) {
+            String[] parts = text.split(",");
+            String part1 = parts[0];
+            String part2 = parts[1];
+            return Integer.parseInt(part1) + Integer.parseInt(part2);
+        } else {
+            return Integer.parseInt(text);
+        }
+    }
 }
