0acb0de3261e163813d7db9f07669457d7929c88
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-03-19 10:01:05 +0200

    Added more tests and Mockito framework

---
+++ src/main/java/info/ernestas/tddplayground/OtherClass.java
@@ -0,0 +1,13 @@
+package info.ernestas.tddplayground;
+
+/**
+ *
+ * @author Ernestas.Kardzys
+ */
+public class OtherClass {
+    
+    public String getResult() {
+        return "Result for OtherClass";
+    }
+    
+}
