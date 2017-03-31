fa7bc63c6d64127e902d695e1fae14ef9833db31
Ernestas Kardzys <Ernestas.Kardzys@intermedix.com>
2017-03-31 22:11:51 +0300

    Added a SuperMarket test

---
+++ src/test/java/info/ernestas/tddplayground/shop/Product.java
@@ -0,0 +1,45 @@
+package info.ernestas.tddplayground.shop;
+
+public class Product {
+
+    private Integer id;
+    private String name;
+
+    public Product(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+
+}
